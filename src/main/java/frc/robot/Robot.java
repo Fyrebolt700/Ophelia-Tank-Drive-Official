679d242ad5b446df8a576081eaa57e03f3c44d6e
Fyrebolt700 <rithanyaj1@gmail.com>
2024-11-05 13:05:39 -0700

    Drivecontrol with 70% speed button

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,5 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
+//import packages
 package frc.robot;
 
 import edu.wpi.first.util.sendable.SendableRegistry;
@@ -12,21 +10,24 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-/**
- * Runs the motors with arcade steering.
- */
+
 public class Robot extends TimedRobot {
 
+  //initialize motors with correct device IDs and motor type
   private final CANSparkMax m_leftMotor1 = new CANSparkMax(1, MotorType.kBrushed);
   private final CANSparkMax m_leftMotor2 = new CANSparkMax(2, MotorType.kBrushed);
 
   private final CANSparkMax m_rightMotor1 = new CANSparkMax(3, MotorType.kBrushed);
   private final CANSparkMax m_rightMotor2 = new CANSparkMax(4, MotorType.kBrushed);
 
+  //initializing tank drive with both motors
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor1, m_rightMotor1);
+
+  //initalizing controller
   private final Joystick m_stick = new Joystick(0);
 
-
+  //adds both motors to registry under parent object(m_robotDrive)
+  //For better organization later on 
   public Robot() {
     SendableRegistry.addChild(m_robotDrive, m_leftMotor1);
     SendableRegistry.addChild(m_robotDrive, m_rightMotor1);
@@ -37,22 +38,28 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
+    // We need to invert both sides
     m_rightMotor1.setInverted(true);
     m_leftMotor1.setInverted(true);
     m_rightMotor2.follow(m_rightMotor1);
     m_leftMotor2.follow(m_leftMotor1);
-
-
   }
 
   @Override
   public void teleopPeriodic() {
     // Drive with arcade drive.
-    // That means that the Y axis drives forward
-    // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(m_stick.getX(), -m_stick.getY());
+    // Y axis drives forward
+    // X turns left and right
+    
+    //m_robotDrive.arcadeDrive(m_stick.getX() * 0.6, m_stick.getY() * 0.6);
+
+    //m_robotDrive.arcadeDrive(-m_stick.getX(), m_stick.getY());
+
+    //if the "RB" button is held down, max speed is 60%
+    if (m_stick.getRawButton(6)){
+      m_robotDrive.arcadeDrive(m_stick.getX()*0.7, -m_stick.getY()*0.7);
+    } else {
+      m_robotDrive.arcadeDrive(m_stick.getX(), -m_stick.getY());
+    }
   }
 }
